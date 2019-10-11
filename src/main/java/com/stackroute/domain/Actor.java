93ca9b47628145f8c497b488689c002a14f273ac
Rishabh-Dwivedi-g <dwivedirishabh02@gmail.com>
2019-10-11 08:08:46 +0530

    Update Actor.java

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -8,13 +8,22 @@ public class Actor {
     public void setName(String name) {
         this.name = name;
     }
+    public String getName() {
+        return name;
+    }
 
     public void setGender(String gender) {
         this.gender = gender;
     }
+    public String getGender() {
+        return gender;
+    }
 
     public void setAge(int age) {
         this.age = age;
+    }
+    public int getAge() {
+        return age;
     }
     public String getDetails(){
         
